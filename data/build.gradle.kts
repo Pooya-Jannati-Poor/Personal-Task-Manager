508f64d4705f8c619e45c06cc3920991292bbf7a
pooya <poya181@gmail.com>
2025-08-14 09:07:49 +0330

    file structure (clean architecture): create clean modules

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "ir.pooyadev.personaltaskmanager"
+    namespace = "ir.pooyadev.data"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "ir.pooyadev.personaltaskmanager"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,11 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(project(":data"))
-    implementation(project(":domain"))
-    implementation(project(":presentation"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Personal Task Manager"
 include(":app")
- 
+include(":domain")
+include(":data")
+include(":presentation")
