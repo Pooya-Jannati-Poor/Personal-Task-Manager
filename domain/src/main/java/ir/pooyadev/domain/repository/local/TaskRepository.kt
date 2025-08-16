60e5e250429b00e8f0f19148ee37e24ec1e6d441
pooya <poya181@gmail.com>
2025-08-17 00:52:31 +0330

    feat (db): split fetchTasks to fetchTasksSortByNewestFirst and fetchTasksSortByOldestFirst with Flow in TaskRepository.kt

--- domain/src/main/java/ir/pooyadev/domain/repository/local/TaskRepository.kt
+++ domain/src/main/java/ir/pooyadev/domain/repository/local/TaskRepository.kt
@@ -1,12 +1,15 @@
 package ir.pooyadev.domain.repository.local
 
 import ir.pooyadev.domain.model.local.Task
+import kotlinx.coroutines.flow.Flow
 
 interface TaskRepository {
 
     suspend fun insertOrUpdateTask(task: Task): Long
 
-    suspend fun fetchTasks(): List<Task>?
+    suspend fun fetchTasksSortByNewestFirst(): Flow<List<Task>>
+
+    suspend fun fetchTasksSortByOldestFirst(): Flow<List<Task>>
 
     suspend fun fetchTasksByTaskId(taskId: Long): Task?
 
