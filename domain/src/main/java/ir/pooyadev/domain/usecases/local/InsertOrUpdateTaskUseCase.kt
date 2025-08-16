589adcada83ea785fda4720d6eff0077701c173c
pooya <poya181@gmail.com>
2025-08-17 01:06:21 +0330

    chor : rename InsertTaskUseCase to InsertOrUpdateTaskUseCase

--- domain/src/main/java/ir/pooyadev/domain/usecases/local/InsertOrUpdateTaskUseCase.kt
+++ domain/src/main/java/ir/pooyadev/domain/usecases/local/InsertOrUpdateTaskUseCase.kt
@@ -4,8 +4,8 @@ import ir.pooyadev.domain.model.local.Task
 import ir.pooyadev.domain.repository.local.TaskRepository
 import javax.inject.Inject
 
-class InsertTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
+class InsertOrUpdateTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
     suspend operator fun invoke(task: Task): Long {
-        return taskRepository.insertTask(task)
+        return taskRepository.insertOrUpdateTask(task)
     }
 }
